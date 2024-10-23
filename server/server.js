f4245128656339cced8696828148170b68178c65
uixPhuke <uixphuke@gmail.com>
2024-10-23 20:36:50 +0530

    database created

--- server/server.js
+++ server/server.js
@@ -1,17 +1,29 @@
-const express=require("express")
-const app=express();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-
-
-const PORT=process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 //server PORT
-app.listen(PORT,()=>{
-    console.log(`Server running at PORT ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running at PORT ${PORT}`);
 });
 
 //get request
-app.get("/",(req,res)=>{
-    res.json({msg:"This is server"})
-})
+app.get("/", (req, res) => {
+  res.json({ msg: "This is server" });
+});
+
+//mongo Connection
+const URI = process.env.MONGO_URL;
+
+mongoose
+  .connect(URI)
+  .then(() => {
+    console.log("Connect to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
