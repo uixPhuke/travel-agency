1b66a0901f7e87ced86cc7625d547fe098af269a
uixPhuke <uixphuke@gmail.com>
2024-10-23 19:55:11 +0530

    server created

--- server/server.js
+++ server/server.js
@@ -3,5 +3,15 @@ const app=express();
 
 
 
-const PORT=5000
-app.listen('/',PORT)
+const PORT=process.env.PORT || 5000;
+app.use(express.json());
+
+//server PORT
+app.listen(PORT,()=>{
+    console.log(`Server running at PORT ${PORT}`);
+});
+
+//get request
+app.get("/",(req,res)=>{
+    res.json({msg:"This is server"})
+})
